800e1bb71728ca0a7cde2d1cd3298854851b2afe
DucMinhCao <minhduc.itus@gmail.com>
2020-01-12 21:32:12 +0700

    Modified seeder

--- seeders/20200111084426-demo-category.js
+++ seeders/20200111084426-demo-category.js
@@ -3,18 +3,23 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
       let data = [
-        { name: 'Men',}
+        { name: 'Men', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Women', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Accessories', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Footwear', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Bay item', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Electronic', imagepath: '/img/home/hero-slide1.png'},
+        { name: 'Food', imagepath: '/img/home/hero-slide1.png'},
       ];
+      data.map(item => {
+        item.createdAt = Sequelize.literal('NOW()');
+        item.updatedAt = Sequelize.literal('NOW()');
+        return item;
+      });
       return queryInterface.bulkInsert('Categories', data,{});
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
+  down: (queryInterface, Sequelize) => {  
       return queryInterface.bulkDelete('People', null, {});
-    */
   }
 };
